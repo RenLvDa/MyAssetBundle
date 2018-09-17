1d3680e43c095807c5034bf2a44472ce9ee5c260
renlvda <1041962748@qq.com>
2018-09-17 15:23:48 +0800

    no message

---
+++ MyAssetBundle/Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class CreateAssetBundle : MonoBehaviour {
+	[MenuItem("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles(){
+		string dir = "AssetBundles";
+		if (Directory.Exists (dir) == false) {
+			Directory.CreateDirectory (dir);
+		}
+		BuildPipeline.BuildAssetBundles (dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+	}
+}
