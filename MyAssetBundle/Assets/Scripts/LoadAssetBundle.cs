02c7c69f9e0aad287056d50b29c718cdbaf67538
renlvda <1041962748@qq.com>
2018-09-17 18:43:13 +0800

    update

---
+++ MyAssetBundle/Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LoadAssetBundle : MonoBehaviour {
+	void OnGUI(){
+		if(GUILayout.Button("LoadAssetBundle")){
+			DateTime t0 = DateTime.Now;
+
+			AssetBundle manifestBundle = AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/Windows/Windows");
+
+			if (manifestBundle != null) {
+				AssetBundleManifest manifest = (AssetBundleManifest) manifestBundle.LoadAsset ("AssetBundleManifest");
+				//TODO 加载
+			}
+		}
+	}
+}
